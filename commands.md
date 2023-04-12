88988a0c3724f66c6c44e368b41c0449874d9cde
Yani <devyani.maladkar@gmail.com>
2023-04-12 12:17:46 -0500

    perplexity score files

--- commands.md
+++ commands.md
@@ -43,7 +43,8 @@ Command to deactivate (Only this nothing else)
 Controlled Generation Command
 ```python3 controlled_text_generation_v2.py --input_file dataset/e2e_data/target_attribute.json --gen_model_path trained_models_text_generation/gpt2_e2e_5.pt --per_control 1 --use_bert True --debug True --lambda_condition 5```
 
-
+Generate Perplexity Scores
+```python3 improved-diffusion/scripts/ppl_under_ar.py --input_text datasets/test_perplexity.txt --model_name_or_path classifier_models/e2e-tgt_e\=5_b\=10_m\=gpt2_wikitext-103-raw-v1_101_finetune_None --mode eval --experiment random --model_path /work/09396/yani28/ls6/Diffusion-LM/classifier_models/e2e-tgt_e\=5_b\=10_m\=gpt2_wikitext-103-raw-v1_101_finetune_None/pytorch_model.bin ``` 
 
 
 
--- convert_files.py
+++ convert_files.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-
+import json
 
 def read_file_lbyl(path):
     lines=[]
@@ -10,24 +10,61 @@ def read_file_lbyl(path):
     
     return lines
 
+def read_jsonl(path):
+    json_list=None
+    with open(path, 'r') as json_file:
+        json_list = list(json_file)
+    return json_list
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='controlled text generation args.')
     parser.add_argument('--input_file', type=str, default='generated_output/controlled_generation.txt', help='')
     parser.add_argument('--output_dir', type=str, default='perplex_score_input/', help='')
     # COMMAND : python3 controlled_text_generation_v2.py --input_file dataset/e2e_data/src1_valid.txt --gen_model_path trained_models_text_generation/gpt2_e2e_5.pt --per_control 1
-
+    eos_tag="<|endoftext|>" # this is for the teacher model
     args = parser.parse_args()
-    
-    file_data=read_file_lbyl(args.input_file)
-    eos_tag="<|endoftext|>"
+    if args.input_file.endswith('.txt'):
+        # fudge output is .txt
+        # each line is a sentence with :
+        # a line : <control> : <value> || <sentence> \n eos_tag
+        print("Reading file ",args.input_file)
+        file_data=read_file_lbyl(args.input_file)
+        
 
-    output_data=[]
-    for line in file_data:
-        if "||" not in line :
-            continue
-        output_data.append(line.strip().split("||")[1].strip()+" "+eos_tag)
-    # print(output_data)
-    output_file=os.path.join(args.output_dir,"perplexity_input_"+os.path.split(args.input_file)[-1])
+        output_data=[]
+        for line in file_data:
+            if "||" not in line :
+                continue
+            output_data.append(line.strip().split("||")[1].strip()+" "+eos_tag)
+        # print(output_data)
+        output_file=os.path.join(args.output_dir,"perplexity_input_"+os.path.split(args.input_file)[-1])
+        
+    elif args.input_file.endswith('.jsonl'):
+        print("Reading file ",args.input_file)
+        file_data=read_jsonl(args.input_file)
+
+        output_data=[]
+        for json_str in file_data:
+            entry = json.loads(json_str)
+            for key in entry.keys():
+                for sent in entry[key]:
+                    print(sent.split())
+                    new_sent=""
+                    for i in sent.split():
+                        if i=='END':
+                            output_data.append(sent.strip()+" "+eos_tag)
+                            sent=""
+                            continue
+                        elif i=='START':
+                            sent=""
+                            continue
+                        sent=sent+" "+i
+        # print(output_data)
+        output_file=os.path.join(args.output_dir,"perplexity_input_"+os.path.split(args.input_file)[-1].split(".jsonl")[0]+".txt")
+
+    else:
+        print("Error reading file")
+    
     with open(output_file, 'w') as f:
         for line in output_data:
             f.write(line)
